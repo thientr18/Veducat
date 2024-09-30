89da3c32d11138ef414ad3a9d6e63e79974b7ea9
Thien Tran <tranthien1882003@gmail.com>
2024-09-30 23:37:23 +0700

    Update
    Initial Project
    Install libraries: expressjs, express-handlebars

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('Hello Testing!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
