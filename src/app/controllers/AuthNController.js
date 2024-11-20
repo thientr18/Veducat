a14ef99daadc8783fa11ecde9ff80c51785b8625
Thien Tran <tranthien1882003@gmail.com>
2024-11-20 11:31:49 +0700

    fix schema

--- src/app/controllers/AuthNController.js
+++ src/app/controllers/AuthNController.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const handleErrors = (err) => {
-    console.error(err.message, err.code);
-    let errors = { username: '', password: '' };
+    console.error(err.message);
+    let errors = { userID: '', password: '' };
   
-    // incorrect username
-    if (err.message === 'incorrect username') {
-        errors.username = 'that username is not registered';
+    // incorrect userID
+    if (err.message === 'incorrect user ID') {
+        errors.userID = 'that user ID is not registered';
     }
   
     // incorrect password
@@ -17,7 +17,7 @@ const handleErrors = (err) => {
   
     // duplicate errors
     if (err.code === 11000) {
-        errors.username = 'this username is already in use'
+        errors.userID = 'this user ID is already in use'
         return errors;
     }
   
@@ -47,10 +47,10 @@ module.exports.login_get = (req, res) => {
 }
   
 module.exports.signup_post = async (req, res) => {
-    const { username, password } = req.body;
+    const { userID, password } = req.body;
     
     try {
-        const user = await User.create({ username, password})
+        const user = await User.create({ userID, password })
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });  // maxAge in milliseconds
         res.status(201).json({ user: user._id });
@@ -62,10 +62,10 @@ module.exports.signup_post = async (req, res) => {
 }
   
 module.exports.login_post = async (req, res) => {
-    const { username, password } = req.body;
+    const { userID, password } = req.body;
   
     try {
-        const user = await User.login(username, password)
+        const user = await User.login(userID, password )
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });  // maxAge in milliseconds
         res.status(200).json({ user: user._id})
