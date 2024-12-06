bec4afe6eee62b77f5cebdc5284876c057f77ffc
Thien Tran <tranthien1882003@gmail.com>
2024-12-06 23:04:57 +0700

    streamline AuthZ and middlewares for routers

--- src/routes/student.js
+++ src/routes/student.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const studentController = require('../app/controllers/StudentController');
 const { requireAuthZ } = require('../app/middlewares/authZ');
-const { checkUser } = require('../app/middlewares/authN');
 const router = express.Router()
 
 // GET /student
-router.get('/course/:_id/announcement', checkUser, studentController.announcement_get);
-router.get('/course/:_id/material', checkUser, studentController.material_get);
-router.get('/course/:_id/contact', checkUser, studentController.contact_get);
-router.get('/course/:_id/homework', checkUser, studentController.homework_get);
-router.get('/course/:_id/discussion', checkUser, studentController.discussion_get);
-router.get('/course/:_id/grade', checkUser, studentController.grade_get);
-router.get('/course/:_id/', checkUser, studentController.course_get);
-router.get('/', checkUser, studentController.index_get);
+router.get('/course/:_id/announcement', requireAuthZ('student'), studentController.announcement_get);
+router.get('/course/:_id/material', requireAuthZ('student'), studentController.material_get);
+router.get('/course/:_id/contact', requireAuthZ('student'), studentController.contact_get);
+router.get('/course/:_id/homework', requireAuthZ('student'), studentController.homework_get);
+router.get('/course/:_id/discussion', requireAuthZ('student'), studentController.discussion_get);
+router.get('/course/:_id/grade', requireAuthZ('student'), studentController.grade_get);
+router.get('/course/:_id/', requireAuthZ('student'), studentController.course_get);
+router.get('/', requireAuthZ('student'), studentController.index_get);
 
 module.exports = router;
