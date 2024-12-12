acd139791267e57835c87cc2cc0ece8312057369
Thien Ngan Le <lethienngan2018@gmail.com>
2024-12-12 20:04:04 +0700

    update homework display

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -46,14 +46,17 @@ router.get('/announcement/', requireAuthZ('teacher'), teacherController.admin_an
 router.get('/course/:_id/material', requireAuthZ('teacher'), teacherController.material_get);
 router.post('/course/:_id/material', cpUpload, requireAuthZ('teacher'), teacherController.material_post);
 router.get('/course/:_id/material/:mID', requireAuthZ('teacher'), teacherController.material_detail_get);
-router.get('/course/:_id/delete/:_id', requireAuthZ('teacher'), teacherController.teacher_delete_material);
-router.put('/course/:_id/edit/:_id', cpUpload, requireAuthZ('teacher'), teacherController.teacher_edit_material);
+router.get('/course/:_id/material/delete/:_id', requireAuthZ('teacher'), teacherController.teacher_delete_material);
+router.put('/course/:_id/material/edit/:_id', cpUpload, requireAuthZ('teacher'), teacherController.teacher_edit_material);
 
 router.get('/course/:_id/contact', requireAuthZ('teacher'), teacherController.contact_get);
 
 router.get('/course/:_id/homework', requireAuthZ('teacher'), teacherController.homework_get);
 router.post('/course/:_id/homework', cpUpload, requireAuthZ('teacher'), teacherController.homework_post);
 router.get('/homework/task', requireAuthZ('teacher'), teacherController.tasks_get);
+router.get('/course/:_id/homework/:hID', requireAuthZ('teacher'), teacherController.homework_detail_get);
+router.get('/course/:_id/homework/delete/:_id', requireAuthZ('teacher'), teacherController.teacher_delete_material);
+router.put('/course/:_id/homework/edit/:_id', cpUpload, requireAuthZ('teacher'), teacherController.teacher_edit_material);
 
 router.get('/course/:_id/discussion', requireAuthZ('teacher'), teacherController.discussion_get);
 router.get('/discussion', requireAuthZ('teacher'), teacherController.discussion_all_get);
